8bc51d5238fda215ac0d6c86b0bedc5068316b59
dennis <egroup.intern.dennis@gmail.com>
2026-05-10 11:37:27 +0800

    fix

--- financial-ai-client/src/utils/config.ts
+++ financial-ai-client/src/utils/config.ts
@@ -1,9 +1,9 @@
 import { ErrorType } from "@/type/dto/dto"
 
-export const FINANCIAL_BACKEND_URL='http://localhost:8080/api'
+export const FINANCIAL_BACKEND_URL = process.env.NEXT_PUBLIC_FINANCIAL_BACKEND_URL ?? 'http://localhost:8080/api'
 
-export const FINANCIAL_AI_URL= 'http://localhost:3000/api'
+export const FINANCIAL_AI_URL = process.env.NEXT_PUBLIC_FINANCIAL_AI_URL ?? 'http://localhost:3000/api'
 
-export  function handleError(e:ErrorType) {
+export function handleError(e: ErrorType) {
     return e.data
 }
