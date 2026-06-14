7268e85447657439c0f39b3f363e51390fa7b911
dennis <egroup.intern.dennis@gmail.com>
2026-06-14 11:11:53 +0800

    修正名稱

--- financial-ai-backend/src/main/java/org/financial/financialaibackend/service/S3Service.java
+++ financial-ai-backend/src/main/java/org/financial/financialaibackend/service/S3Service.java
@@ -17,7 +17,7 @@ public class S3Service {
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
-    @Value("${spring.cloud.aws.region}")
+    @Value("${spring.cloud.aws.region.static}") 
     private String region;
 
     public String upload(MultipartFile file,String folderName,String fileName) throws java.io.IOException {
