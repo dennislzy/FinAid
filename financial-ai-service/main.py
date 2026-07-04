f2d002f688de3967f981326a2a3098420b145fcc
dennis <egroup.intern.dennis@gmail.com>
2026-07-04 11:35:19 +0800

    fix

--- financial-ai-service/main.py
+++ financial-ai-service/main.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from fastapi import FastAPI, UploadFile
 from fastapi.params import File
 from fastapi.responses import JSONResponse
-from  controller import audioAndSummaryController
-from controller import ChatController
-from controller import testController
+# from  controller import audioAndSummaryController
+# from controller import ChatController
+# from controller import testController
 from controller import AIController
 app = FastAPI(title="簡單的 FastAPI 應用")
 import typer
@@ -17,7 +17,7 @@ async def root():
     return {"message": "歡迎使用 FastAPI"}
 
 
-app.include_router(audioAndSummaryController.router)
+# app.include_router(audioAndSummaryController.router)
 # app.include_router(ChatController.router)
 # app.include_router(testController.router)
 app.include_router(AIController.router)
