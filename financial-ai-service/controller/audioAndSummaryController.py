7aeff3551dcc9c1e93e141f4096cf3662e1707d7
dennis <egroup.intern.dennis@gmail.com>
2026-07-04 11:26:41 +0800

    fix

--- financial-ai-service/controller/audioAndSummaryController.py
+++ financial-ai-service/controller/audioAndSummaryController.py
@@ -1,160 +1,160 @@
-import os
-from fastapi import FastAPI
-import whisper
-import multiprocessing
-from fastapi import APIRouter, File, UploadFile
-from pydantic import BaseModel
-import whisper
-import tempfile
-from datetime import datetime
-from service.audioToTextService import AudioToTextService
-from VectorStore.FaissVectorStore import CustomFaissVectorStore, DataType
-from fastapi.responses import JSONResponse
-from langchain_core.documents import Document
+# import os
+# from fastapi import FastAPI
+# import whisper
+# import multiprocessing
+# from fastapi import APIRouter, File, UploadFile
+# from pydantic import BaseModel
+# import whisper
+# import tempfile
+# from datetime import datetime
+# from service.audioToTextService import AudioToTextService
+# from VectorStore.FaissVectorStore import CustomFaissVectorStore, DataType
+# from fastapi.responses import JSONResponse
+# from langchain_core.documents import Document
 
-from utils.splitterUtil import split_datas
-router = APIRouter(prefix="/api/ai", tags=['audio'])
-
-
-class AudioTextRequest(BaseModel):
-    audioTexts: str
+# from utils.splitterUtil import split_datas
+# router = APIRouter(prefix="/api/ai", tags=['audio'])
 
 
-class FileRequest(BaseModel):
-    file_path: str
+# class AudioTextRequest(BaseModel):
+#     audioTexts: str
 
 
-class SummaryRequest(BaseModel):
-    summaryTexts: str
-    fileName: str
+# class FileRequest(BaseModel):
+#     file_path: str
 
 
-WHISPER_MODEL_DIR = '/app/whisper_models'
-os.makedirs(WHISPER_MODEL_DIR, exist_ok=True)
+# class SummaryRequest(BaseModel):
+#     summaryTexts: str
+#     fileName: str
 
 
-# 修改 Whisper 的下载根目录
-whisper.utils.download_root = WHISPER_MODEL_DIR
-
-app = FastAPI()
-
-# 使用多进程锁来确保只加载一次
-model_lock = multiprocessing.Lock()
-MODEL = None
+# WHISPER_MODEL_DIR = '/app/whisper_models'
+# os.makedirs(WHISPER_MODEL_DIR, exist_ok=True)
 
 
-def get_model():
-    global MODEL
-    if MODEL is None:
-        with model_lock:
-            if MODEL is None:  # 双重检查锁定
-                print("正在加载 Whisper 模型...")
-                MODEL = whisper.load_model(
-                    "small", download_root=WHISPER_MODEL_DIR)
-                print("Whisper 模型加载完成")
-    return MODEL
+# # 修改 Whisper 的下载根目录
+# whisper.utils.download_root = WHISPER_MODEL_DIR
+
+# app = FastAPI()
+
+# # 使用多进程锁来确保只加载一次
+# model_lock = multiprocessing.Lock()
+# MODEL = None
 
 
-# 在主进程启动时加载模型
-MODEL = get_model()
+# def get_model():
+#     global MODEL
+#     if MODEL is None:
+#         with model_lock:
+#             if MODEL is None:  # 双重检查锁定
+#                 print("正在加载 Whisper 模型...")
+#                 MODEL = whisper.load_model(
+#                     "small", download_root=WHISPER_MODEL_DIR)
+#                 print("Whisper 模型加载完成")
+#     return MODEL
 
 
-@router.post("/audio/{case_name}")
-async def audioToText(audio_file: UploadFile = File(...), case_name: str = None):
-    vector_store = CustomFaissVectorStore(data_type=DataType.DOCUMENT)
-    audio_service = AudioToTextService(None)
-    try:
-        start_time = datetime.now()
-        model = get_model()  # 获取已加载的模型
-
-        # 检查文件类型
-        if not audio_file.content_type.startswith('audio/'):
-            return JSONResponse(
-                status_code=400,
-                content={"error": "请上传音频文件"}
-            )
-
-        # 创建临时文件来存储上传的音频
-        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(audio_file.filename)[1]) as temp_file:
-            content = await audio_file.read()
-            temp_file.write(content)
-            temp_path = temp_file.name
-
-        try:
-            result = model.transcribe(temp_path)
-            end_time = datetime.now()
-            process_time = (end_time - start_time) * 1000
-            # punctuated_text = audio_service.add_punctuation(result['text'])
-            # split_doc = split_datas(documents=[Document(
-            #     page_content=punctuated_text)], chunk_size=500, chunk_overlap=10, data_type='documents')
-            # vector_store.add_documents(file_name=case_name, datas=split_doc)
-            return result
-
-        finally:
-            os.unlink(temp_path)
-
-    except Exception as e:
-        return JSONResponse(
-            status_code=500,
-            content={
-                "status": "error",
-                "message": str(e)
-            }
-        )
-
-@router.post("/summary")
-async def summary(audioRequest: AudioTextRequest):
-
-    audio_service = AudioToTextService(None)
-
-    result = audio_service.generate_summary(audioRequest.audioTexts)
-
-    return result
-
-# 新增 /punctuate 端點
+# # 在主进程启动时加载模型
+# MODEL = get_model()
 
 
-@router.post("/punctuate")
-async def punctuate_text(request: AudioTextRequest):
-    try:
-        text = request.audioTexts
-        if not text:
-            return JSONResponse(status_code=400, content={"error": "文字內容不能為空"})
-        audio_service = AudioToTextService(None)
-        punctuated_text = audio_service.add_punctuation(text)
-        return {"result": punctuated_text}
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"status": "error", "message": str(e)})
+# @router.post("/audio/{case_name}")
+# async def audioToText(audio_file: UploadFile = File(...), case_name: str = None):
+#     vector_store = CustomFaissVectorStore(data_type=DataType.DOCUMENT)
+#     audio_service = AudioToTextService(None)
+#     try:
+#         start_time = datetime.now()
+#         model = get_model()  # 获取已加载的模型
+
+#         # 检查文件类型
+#         if not audio_file.content_type.startswith('audio/'):
+#             return JSONResponse(
+#                 status_code=400,
+#                 content={"error": "请上传音频文件"}
+#             )
+
+#         # 创建临时文件来存储上传的音频
+#         with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(audio_file.filename)[1]) as temp_file:
+#             content = await audio_file.read()
+#             temp_file.write(content)
+#             temp_path = temp_file.name
+
+#         try:
+#             result = model.transcribe(temp_path)
+#             end_time = datetime.now()
+#             process_time = (end_time - start_time) * 1000
+#             # punctuated_text = audio_service.add_punctuation(result['text'])
+#             # split_doc = split_datas(documents=[Document(
+#             #     page_content=punctuated_text)], chunk_size=500, chunk_overlap=10, data_type='documents')
+#             # vector_store.add_documents(file_name=case_name, datas=split_doc)
+#             return result
+
+#         finally:
+#             os.unlink(temp_path)
+
+#     except Exception as e:
+#         return JSONResponse(
+#             status_code=500,
+#             content={
+#                 "status": "error",
+#                 "message": str(e)
+#             }
+#         )
+
+# @router.post("/summary")
+# async def summary(audioRequest: AudioTextRequest):
+
+#     audio_service = AudioToTextService(None)
+
+#     result = audio_service.generate_summary(audioRequest.audioTexts)
+
+#     return result
+
+# # 新增 /punctuate 端點
 
 
-@router.get("/get_vector/{file_name}")
-async def get_vector(file_name: str):
-    vector_store = CustomFaissVectorStore()
+# @router.post("/punctuate")
+# async def punctuate_text(request: AudioTextRequest):
+#     try:
+#         text = request.audioTexts
+#         if not text:
+#             return JSONResponse(status_code=400, content={"error": "文字內容不能為空"})
+#         audio_service = AudioToTextService(None)
+#         punctuated_text = audio_service.add_punctuation(text)
+#         return {"result": punctuated_text}
+#     except Exception as e:
+#         return JSONResponse(status_code=500, content={"status": "error", "message": str(e)})
 
-    return vector_store.get_doc(file_name=file_name)
+
+# @router.get("/get_vector/{file_name}")
+# async def get_vector(file_name: str):
+#     vector_store = CustomFaissVectorStore()
+
+#     return vector_store.get_doc(file_name=file_name)
 
 
-@router.delete("/delete_vector/{file_name}")
-async def delete_doc(file_name: str):
+# @router.delete("/delete_vector/{file_name}")
+# async def delete_doc(file_name: str):
 
-    vector_store = CustomFaissVectorStore(data_type=DataType.DOCUMENT)
+#     vector_store = CustomFaissVectorStore(data_type=DataType.DOCUMENT)
 
-    vector_store.delete_documents(file_name=file_name)
+#     vector_store.delete_documents(file_name=file_name)
 
-    return "刪除成功"
+#     return "刪除成功"
 
-@router.post("/rebuild-vector/{file_name}")
-async def rebuild(audio_text:AudioTextRequest,file_name:str):
+# @router.post("/rebuild-vector/{file_name}")
+# async def rebuild(audio_text:AudioTextRequest,file_name:str):
 
-    vector_store = CustomFaissVectorStore(data_type=DataType.DOCUMENT)
+#     vector_store = CustomFaissVectorStore(data_type=DataType.DOCUMENT)
 
-    vector_store.delete_documents(file_name=file_name)
+#     vector_store.delete_documents(file_name=file_name)
 
-    split_doc = split_datas(documents=[Document(
-        page_content=audio_text.audioTexts)], chunk_size=500, chunk_overlap=10, data_type='documents'
-    )
+#     split_doc = split_datas(documents=[Document(
+#         page_content=audio_text.audioTexts)], chunk_size=500, chunk_overlap=10, data_type='documents'
+#     )
 
-    vector_store.add_documents(file_name=file_name,datas=split_doc)
+#     vector_store.add_documents(file_name=file_name,datas=split_doc)
 
-    return "ok"
+#     return "ok"
     
--- financial-ai-service/service/audioToTextService.py
+++ financial-ai-service/service/audioToTextService.py
@@ -1,57 +1,57 @@
-from langchain_core.prompts import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
-from langchain_core.documents import Document
-from VectorStore.FaissVectorStore import CustomFaissVectorStore, DataType
-from modal.AIModal import AIModal
-from prompts import combine_prompt, summary_prompt
-from utils.splitterUtil import split_datas
+# from langchain_core.prompts import PromptTemplate
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.chains.summarize import load_summarize_chain
+# from langchain_core.documents import Document
+# from VectorStore.FaissVectorStore import CustomFaissVectorStore, DataType
+# from modal.AIModal import AIModal
+# from prompts import combine_prompt, summary_prompt
+# from utils.splitterUtil import split_datas
 
 
-class AudioToTextService(AIModal):
-    vector_database = CustomFaissVectorStore(data_type=DataType.TEXT)
+# class AudioToTextService(AIModal):
+#     vector_database = CustomFaissVectorStore(data_type=DataType.TEXT)
 
-    def split_doc(self, orginal_doc: str):
-        token_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=300,
-            chunk_overlap=20
-        )
-        chunks = token_splitter.split_text(orginal_doc)
-        doc_chunks = [Document(page_content=chunk) for chunk in chunks]
-        return doc_chunks
+#     def split_doc(self, orginal_doc: str):
+#         token_splitter = RecursiveCharacterTextSplitter(
+#             chunk_size=300,
+#             chunk_overlap=20
+#         )
+#         chunks = token_splitter.split_text(orginal_doc)
+#         doc_chunks = [Document(page_content=chunk) for chunk in chunks]
+#         return doc_chunks
 
-    def generate_summary(self, orginal_doc):
-        chunks_doc = self.split_doc(orginal_doc)
-        summary = PromptTemplate.from_template(summary_prompt)
-        combine = PromptTemplate.from_template(combine_prompt)
+#     def generate_summary(self, orginal_doc):
+#         chunks_doc = self.split_doc(orginal_doc)
+#         summary = PromptTemplate.from_template(summary_prompt)
+#         combine = PromptTemplate.from_template(combine_prompt)
 
-        chain = load_summarize_chain(
-            llm=self.chatModal,
-            chain_type="map_reduce",
-            map_prompt=summary,
-            combine_prompt=combine,
-            verbose=True
-        )
+#         chain = load_summarize_chain(
+#             llm=self.chatModal,
+#             chain_type="map_reduce",
+#             map_prompt=summary,
+#             combine_prompt=combine,
+#             verbose=True
+#         )
 
-        result = chain.invoke(chunks_doc)
-        return result["output_text"]
+#         result = chain.invoke(chunks_doc)
+#         return result["output_text"]
 
-    def add_punctuation(self, text):
-        # 定義 Prompt，要求 LLM 添加標點符號
-        prompt_template = PromptTemplate.from_template(
-            "請幫我將以下逐字稿內容轉換成具可讀性的完整對話，保持原始文字，適度加上標點與換行。角色名稱請加上社工與個案，不可刪減內容，也不可憑空新增內容。：\n\n{text}"
-        )
-        prompt = prompt_template.format(text=text)
+#     def add_punctuation(self, text):
+#         # 定義 Prompt，要求 LLM 添加標點符號
+#         prompt_template = PromptTemplate.from_template(
+#             "請幫我將以下逐字稿內容轉換成具可讀性的完整對話，保持原始文字，適度加上標點與換行。角色名稱請加上社工與個案，不可刪減內容，也不可憑空新增內容。：\n\n{text}"
+#         )
+#         prompt = prompt_template.format(text=text)
 
-        # 使用 chatModal 處理文字
-        result = self.chatModal.invoke(prompt)
-        return result.content  # 返回 LLM 生成的帶標點文字
+#         # 使用 chatModal 處理文字
+#         result = self.chatModal.invoke(prompt)
+#         return result.content  # 返回 LLM 生成的帶標點文字
 
-    def add_texts_to_index(self, texts):
-        split_texts = split_datas(texts)
-        self.vector_database.add_documents(
-            file_name="audio_text", datas=split_texts)
+#     def add_texts_to_index(self, texts):
+#         split_texts = split_datas(texts)
+#         self.vector_database.add_documents(
+#             file_name="audio_text", datas=split_texts)
 
-    def delete_texts(self, file_name: str):
-        self.vector_database.delete_documents(file_name=file_name)
-        return "刪除成功"
+#     def delete_texts(self, file_name: str):
+#         self.vector_database.delete_documents(file_name=file_name)
+#         return "刪除成功"
